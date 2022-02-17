3844eba7ae5e7c4bdea5028b164e1c1709dc6e93
Abiinkun <pabhinn771@outlook.com>
2022-02-17 13:42:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function onclickintro() {
+    window.location.href="index.html";
+}
+function onclickproject(){
+    window.location.href="project.html";
+}
+function onclickcontact(){
+    window.location.href="contact.html";
+}
+function linkGithub(){
+    window.location.href="https://github.com/EMPTYNES"
+}
